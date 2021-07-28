ef57e20c5563772da65b0c471d25eba473f6571a
Dmitri Astafiev <demius.md@gmail.com>
2021-07-28 11:58:24 +0300

    temporary delete generator

--- src/main/java/com/acc/gluon/annotations/RSConstructorProcessor.java
+++ src/main/java/com/acc/gluon/annotations/RSConstructorProcessor.java
@@ -21,46 +21,18 @@ public class RSConstructorProcessor extends AbstractProcessor {
 
     /** public for ServiceLoader */
     public RSConstructorProcessor() {
-        System.out.println("RSConstructorProcessor new");
+        // System.out.println("RSConstructorProcessor new");
     }
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         System.out.println("RSConstructorProcessor process");
         for (var e : roundEnv.getElementsAnnotatedWith(ResultSetConstructor.class)) {
-            // processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE,"Element kind: " + e.getKind().name());
             if (e.getKind() == ElementKind.RECORD) {
                 String name = e.getSimpleName().toString();
                 TypeElement clazz = (TypeElement) e.getEnclosingElement();
 
-                try {
-                    JavaFileObject f = processingEnv.getFiler().
-                            createSourceFile(clazz.getQualifiedName() + "Extras");
-                    processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE,
-                            "Creating " + f.toUri());
-                    Writer w = f.openWriter();
-                    try {
-                        PrintWriter pw = new PrintWriter(w);
-                        pw.println("package "
-                                + clazz.getEnclosingElement().getSimpleName() + ";");
-                        pw.println("public abstract class "
-                                + clazz.getSimpleName() + "Extras {");
-                        pw.println("    protected " + clazz.getSimpleName()
-                                + "Extras() {}");
-                        TypeMirror type = e.asType();
-                        pw.println("    /** Handle something. */");
-                        pw.println("    protected final void handle" + name
-                                + "(" + type + " value) {");
-                        pw.println("        System.out.println(value);");
-                        pw.println("    }");
-                        pw.println("}");
-                        pw.flush();
-                    } finally {
-                        w.close();
-                    }
-                } catch (IOException x) {
-                    processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, x.toString());
-                }
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE,"Element kind: " + e.getKind().name());
             } else {
                 processingEnv.getMessager().printMessage(Diagnostic.Kind.WARNING,"Element kind: " + e.getKind().name() + " is not a RECORD");
                 continue;
