d59fd7b6a3ad4fe0493bad0bceb14ee445630e24
Dmitri Astafiev <demius.md@gmail.com>
2021-08-03 10:06:59 +0300

    use standard NoContentException

--- src/main/java/com/acc/gluon/mappers/NoContentExceptionMapper.java
+++ src/main/java/com/acc/gluon/mappers/NoContentExceptionMapper.java
@@ -1,5 +1,6 @@
 package com.acc.gluon.mappers;
 
+import javax.ws.rs.core.NoContentException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
