d9092e340a2f9f5c7a5f24966cbcf614322a91b4
Dmitri Astafiev <demius.md@gmail.com>
2021-08-03 08:36:43 +0300

    add NoContentException default constructor

--- src/main/java/com/acc/gluon/mappers/NoContentException.java
+++ src/main/java/com/acc/gluon/mappers/NoContentException.java
@@ -3,6 +3,10 @@ package com.acc.gluon.mappers;
 import javax.ws.rs.WebApplicationException;
 
 public class NoContentException extends WebApplicationException {
+    public NoContentException() {
+        super("Nu s-a găsit nimic pentru solicitarea dvs.");
+    }
+
     public NoContentException(String message) {
         super(message);
     }
