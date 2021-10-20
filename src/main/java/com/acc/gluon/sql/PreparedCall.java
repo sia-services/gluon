df43864a7b084fb804ea69a3cea8f140d6caa145
Dmitri Astafiev <demius.md@gmail.com>
2021-10-20 08:16:21 +0300

    once prepare call with provided preparator

--- src/main/java/com/acc/gluon/sql/PreparedCall.java
+++ src/main/java/com/acc/gluon/sql/PreparedCall.java
@@ -7,13 +7,12 @@ import java.sql.SQLException;
 public class PreparedCall<V> implements AutoCloseable {
     private final CallableStatement st;
     private final CallableStatementExtractor<V> extractor;
-    private final CallableStatementPreparator preparator;
 
     public PreparedCall(Connection connection, String sql, CallableStatementExtractor<V> extractor, CallableStatementPreparator preparator) throws SQLException {
         String command = "{call " + sql + "}";
         this.st = connection.prepareCall(command);
         this.extractor = extractor;
-        this.preparator = preparator;
+        preparator.prepareCall(st);
     }
 
     @Override
@@ -26,7 +25,6 @@ public class PreparedCall<V> implements AutoCloseable {
     }
 
     public V call(CallableStatementPreparator preparator) throws SQLException {
-        if (this.preparator != null) this.preparator.prepareCall(st);
         if (preparator != null) preparator.prepareCall(st);
         var ok = st.execute();
         return extractor.extract(st);
